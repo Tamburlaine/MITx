9e30257c8033a7783266a19cde0f9b1bd2f8ef0f
Kate Roe <cityiguana@gmail.com>
2013-06-19 11:10:57 -0400

    with margins

--- visual.js
+++ visual.js
@@ -1,31 +1,35 @@
 var data = [0, 4, 8, 8, 15, 16, 23, 42];
 
-var chart_height = 140;
+var outer_width = 300;
+var outer_height = 300;
+
+var margin = {top: 20, right: 20, bottom: 20, left: 20};
+
+var chart_width = outer_width - margin.left - margin.right;
+var chart_height = outer_height - margin.top - margin.bottom;
 
 //function that maps domain values to range values
-var x_scale = d3.scale.linear().domain([0, d3.max(data)]).range(["0%", "100%"]);
-var y_scale = d3.scale.ordinal().domain(d3.keys(data))
-	.rangeBands([0, chart_height]);
+var x_scale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chart_width]);
+var y_scale = d3.scale.linear().domain([0, d3.max(data)])
+	.range([0, chart_height]);
 
-var chart = d3.select(".chart-container")
-			.append("svg")
-			.attr("class", "chart");
-chart.selectAll("div").data(data)
-	.enter().append("div")
-	.style("width", x_scale)
-	.text(function(d) {return d;});
+var chart = d3.select(".chart-container").append("svg")
+			.attr("class", "chart")
+			.attr("height", outer_height)
+			.attr("width", outer_width)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
 chart.selectAll("rect")
 	.data(data).enter().append("rect")
-	.attr("y", function(d, i){return y_scale(i);}) //
-	.attr("width", x_scale)
-	.attr("height",20);
+	.attr("x", function(d,i) {return x_scale(i);})
+	.attr("y", function(d) {return chart_height - y_scale(d);}) //
+	.attr("width", x_scale.rangeBand())
+	.attr("height", y_scale);
 
 chart.selectAll("text").data(data)
 	.enter().append("text")
-	.attr("x", x_scale)
-	.attr("y", function(d, i) {return y_scale(i) + y_scale.rangeBand()/1.3; })
-	.attr("dx", -3)
-	.attr("dy", "-0.35em")
-	.attr("text-anchor", "end")
+	.attr("x", function(d, i) {return x_scale(i) + x_scale.rangeBand()/2;})
+	.attr("y", function(d) {return chart_height - y_scale(d);})
+	.attr("dy", "1em")
 	.text(function (d) {return d;})
